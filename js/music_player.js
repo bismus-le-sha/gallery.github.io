855a244d49fedd1b03d850655ea93f840c87d78b
Bismus <alexeybismus@gmail.com>
2025-01-16 22:26:44 +0300

    feat: music ref...

--- js/music_player.js
+++ js/music_player.js
@@ -1,4 +1,4 @@
-const audio = new Audio("/music/allways forever - slowed.wav");
+const audio = new Audio("../music/allways forever - slowed.wav");
 audio.loop = true;
 const checkboxInput = document.getElementById("checkboxInput");
 checkboxInput.addEventListener("change", () => {
