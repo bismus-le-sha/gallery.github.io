525b880bc16d287cb2705a0a5e58bc784778fd5a
Bismus <alexeybismus@gmail.com>
2025-01-16 22:42:15 +0300

    pew pew

--- js/music_player.js
+++ js/music_player.js
@@ -1,4 +1,4 @@
-const audio = new Audio("/music/allways_forever_slowed.wav");
+const audio = new Audio("music/allways_forever_slowed.wav");
 audio.loop = true;
 const checkboxInput = document.getElementById("checkboxInput");
 checkboxInput.addEventListener("change", () => {
